638586264656522c6333fe4ea7eb7f51389f2d52
celentanoAlexey <44639359+celentanoAlexey@users.noreply.github.com>
2023-02-09 16:57:54 +0200

    Update task_runner.rb

--- task_runner.rb
+++ task_runner.rb
@@ -6,14 +6,12 @@ smb_client  = SMBClient.new('smb://192.***.**.*', 'Oleksii', '512312')
 
 upload_entry_name = "1_level_folder"
 
-# if smb_client.upload_entity(upload_entry_name)
-#   smb_client.logger.debug("'#{upload_entry_name}' folder has been uploaded")
-# else
-#   smb_client.logger.debug("#{upload_entry_name} can't be uploaded")
-# end
-# smb_client.upload_entity(upload_entry_name)
+if smb_client.upload_entity(upload_entry_name)
+  smb_client.logger.debug("'#{upload_entry_name}' folder has been uploaded")
+else
+  smb_client.logger.debug("#{upload_entry_name} can't be uploaded")
+end
 
-smb_client.upload_entity(upload_entry_name)
 
 
 # smb_client.disconnect
